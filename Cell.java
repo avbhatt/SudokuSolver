024ef3d1f9c99a2c0644c29ea625f04d40bfab20
Adit Bhatt <avbhatt@umich.edu>
2017-09-22 00:12:39 -0400

    feat: create Stack to hold guesses throughout iteration feat: add guess variables to hold alternate values, set status, and possibilities

--- Cell.java
+++ Cell.java
@@ -1,16 +1,27 @@
 package com.adit;
 
 import java.util.HashSet;
+import java.util.Stack;
 
+/*
+Need a stack to hold guessPossible sets
+peek into set to analyze
+pop off to go back to previous guessPossible */
 public class Cell {
     private int value;
     private Boolean set;
     private HashSet<Integer> possible;
+    private int guessValue;
+    private Boolean guessSet;
+    private Stack<HashSet<Integer>> guesses;
+    //private HashSet<Integer> guessPossible;
+
     public Cell(int val){
         value = val;
         if (val != 0)
             set = true;
         else set = false;
+        guesses = new Stack<>();
     }
 
     public Integer getValue() {
@@ -19,11 +30,9 @@ public class Cell {
 
     public void setValue(int value) {
         this.value = value;
+        this.set = true;
     }
 
-    public void setSet(Boolean set) {
-        this.set = set;
-    }
 
     public Boolean getSet() {
         return set;
@@ -37,8 +46,50 @@ public class Cell {
         return possible;
     }
 
-    @Override
-    public String toString() {
-        return value + "";
+    public int getGuessValue() {
+        return guessValue;
+    }
+
+    public void setGuessValue(int guessValue) {
+        this.guessValue = guessValue;
+        this.guessSet = true;
+    }
+
+    public Boolean getGuessSet() {
+        if (guessSet == null)
+            guessSet = false;
+        return guessSet;
+    }
+
+   /* public HashSet<Integer> getGuessPossible() {
+        return guessPossible;
+    }
+
+    public void setGuessPossible(HashSet<Integer> guessPossible) {
+        this.guessPossible = guessPossible;
+    }*/
+
+    public HashSet<Integer> getGuess() {
+        if (guesses.empty()){
+            return null;
+        }
+        else return guesses.peek();
+    }
+
+    public void addGuess(HashSet<Integer> guess) {
+        this.guesses.push(guess);
+    }
+
+    public Boolean emptyStack() {
+        return this.guesses.empty();
+    }
+
+    public void resetGuessPossible(){
+        this.guesses.pop();
+    }
+
+    public void resetGuess() {
+        this.guessValue = 0;
+        this.guessSet = false;
     }
 }
